6ebaf5d8080041955a2cd17f925b2938c879872a
mark <mark@ubuntu>
2013-10-14 09:09:31 +1000

    tidied up logic jquery

--- jquery/logic.js
+++ jquery/logic.js
@@ -1,63 +1,64 @@
 var last = '';
 var timeOut;
 
-function getTweets(id){
-		$.getJSON("Server.php?start="+id,
-		function(data){
-				$.each(data, function(count,item){
-						addNew(item);
-						last = item.id;
-				});
-		});
+function getTweets(id) {
+    $.getJSON("Server.php?start=" + id,
+            function(data) {
+                $.each(data, function(count, item) {
+                    addNew(item);
+                    last = item.id;
+                });
+            });
 }
 
-function addNew(item){
-		if($('#tweets div.tweet').length>9){ //If we have more than nine tweets
-				$('#tweets div.tweet:first').toggle(300);//remove it form the screen
-				$('#tweets div.tweet:first').removeClass('tweet');//and it's class
-				$("#tweets div:hidden").remove(); //sweeps the already hidden elements
-		}
-		$('#tweets').append(renderTweet(item, 'hidden'));
+function addNew(item) {
+    if ($('#tweets div.tweet').length > 9) { //If we have more than nine tweets
+        $('#tweets div.tweet:first').toggle(300);//remove it form the screen
+        $('#tweets div.tweet:first').removeClass('tweet');//and it's class
+        $("#tweets div:hidden").remove(); //sweeps the already hidden elements
+    }
+    renderTweet(item);
 }
 
-function renderTweet(item){
-		importanceColor=getImportanceColor(item.followers_count);
-                sentimentColor=getSentimentColor(item.sentiment);
-		return '<div class="tweet" id="'+item.id+'">'+
-                        '<img src="' + item.profile_image_url + '">' +
-                
-		'<strong><a href="http://twitter.com/'+item.screen_name+'" style="color:'+importanceColor+'">'+
-		item.screen_name+'</a></strong><span class="text">'+
-		item.text
-		+'</span><span class="created_at"><br /><a href="http://twitter.com/'+
-		item.screen_name+'/status/'+item.id+'">'+
-		item.created_at+'</a></span>'+ '<span class="sentiment"><br /><br /><b>Sentiment Analysis: </b><p style="color:'+sentimentColor+'">'+item.sentiment+'</p></span></div>';
+function renderTweet(item) {
+    var importanceColor = getImportanceColor(item.followers_count);
+    var sentimentColor = getSentimentColor(item.sentiment);
+    var imageLink = "http://twitter.com/" + item.screen_name;
+    var createdLink = "http://twitter.com/" + item.screen_name + "/status/" + item.id;
 
+    $("#tweets")
+    .append($("<div>").addClass("tweet").attr("id", item.id)
+    .append($("<img>").attr("src", item.profile_image_url).addClass("image"))
+    .append($("<a>").attr("href", imageLink).append(item.screen_name).attr("style", "color:" + importanceColor))
+    .append($("<p>").append(item.text).addClass("tweetText"))
+    .append($("<p>").append("<br>created at ").append(item.created_at).addClass("created"))
+    .append($("<p>").addClass("sentiment").append("Sentiment Analysis: ").append(item.sentiment).attr("style", "color:" + sentimentColor))
+    );
 }
 
-function getImportanceColor(number){
-		rgb = 255-Math.floor(16*(Math.log(number+1)+1)); //should return about 0 for 0 followers and 255 for 4million (Ashton Kutchner? Obama?)
-		return 'rgb('+rgb+',0,0)';
+function getImportanceColor(number) {
+    rgb = 255 - Math.floor(16 * (Math.log(number + 1) + 1)); //should return about 0 for 0 followers and 255 for 4million (Ashton Kutchner? Obama?)
+    return 'rgb(' + rgb + ',0,0)';
 }
 
-function getSentimentColor(text){
-		if(text === "positive") {
-                    color="green";
-                } else if (text === "negative") {
-                    color="red";
-                } else if (text === "neutral") {
-                    color="grey";
-                } else {
-                    color="black";
-                }
-                return color;
+function getSentimentColor(text) {
+    if (text === "positive") {
+        color = "green";
+    } else if (text === "negative") {
+        color = "red";
+    } else if (text === "neutral") {
+        color = "grey";
+    } else {
+        color = "black";
+    }
+    return color;
 }
 
-function poll(){
-		timeOut = setTimeout('poll()', 800);//It calls itself every 200ms
-		getTweets(last);
+function poll() {
+    timeOut = setTimeout('poll()', 800);//It calls itself every 200ms
+    getTweets(last);
 }
 
 $(document).ready(function() {
-		poll();		
+    poll();
 });
