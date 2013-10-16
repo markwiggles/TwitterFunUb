f05321c8dfb7f71fed8b078651e37f7bb21aa52d
sumisa91 <sumisa91@ubuntu>
2013-10-16 18:46:59 +1000

    Get Demo is working!

--- awsGetDemo.php
+++ awsGetDemo.php
@@ -1,29 +1,24 @@
 <?php
-//require 'AWSSDKforPHP/aws.phar';
-require 'vendor/autoload.php';
+require 'AWSSDKforPHP/aws.phar';
 
 use Aws\DynamoDb\DynamoDbClient;
-echo 'sachi';
+
 $client = DynamoDbClient::factory(array(
     'key'    => 'AKIAIK7RCPMWTZVQWJIA',
     'secret' => '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',
     'region' => 'us-west-2'
 ));
 
-//$iterator = new ItemIterator($client->getScanIterator(array('TableName' => 'tweets')));
-//// Each item will contain the attributes we added
-//foreach ($iterator as $item) {
-//    // Grab the time number value
-//    echo $item['followers_count'] . "\n";
-//    // Grab the error string value
-//    echo $item->get('text') . "\n";
-//}
-echo 'hi';
 $result = $client->scan(array(
     'TableName' => 'tweets',
 ));
-echo 'hello';
+
+//var_dump($result);
 foreach ($result['Items'] as $item) {
     // Do something with the $item
+    //var_dump($item);
+    //echo $item->text;
+    echo $item['text']['S'];
+    echo $item['followers_count']['N'];
 }
 ?>
